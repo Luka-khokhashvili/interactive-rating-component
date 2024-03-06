f1e09d05dbaafcfc671b5382965c98b466ec2cd8
luka khokhashvili <119745295+Luka-khokhashvili@users.noreply.github.com>
2024-03-06 17:41:54 +0400

    add thank you page

---
+++ src/thanks.js
@@ -0,0 +1,34 @@
+import "./thanks.css";
+import ThankYouImage from "./images/illustration-thank-you.svg";
+
+function ThanksPage() {
+  return (
+    <div className="Thanks-body">
+      <img src={ThankYouImage} alt="Thank you ilustration" />
+      <ScoreDisplay />
+      <ThankYouText />
+    </div>
+  );
+}
+
+function ScoreDisplay(props) {
+  return (
+    <div className="score-display-body">
+      You selected {props.score} out of 5
+    </div>
+  );
+}
+
+function ThankYouText() {
+  return (
+    <div className="thank-text-body">
+      <h1 className="main-title">Thank you!</h1>
+      <p className="paragraph">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
+    </div>
+  );
+}
+
+export default ThanksPage;
